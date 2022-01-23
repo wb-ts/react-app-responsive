b7258f7ec9f50a6da7da9c3b1e321b0990c13fcc
SenDev001 <sergeilipovsky@gmail.com>
2022-01-24 01:15:23 +0300

    inite commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>
+        <div className='social-media'>
+          <a href='https://instagram.com'>
+            <img src='/assets/1.svg' alt='cant fine file!'></img>
+          </a>
+          <a href='https://discord.gg'>
+            <img src='/assets/2.svg' alt='cant fine file!'></img>
+          </a>
+          <img src='/assets/3.svg' alt='cant fine file!'></img>
+          <a href='https://twitter.com'>
+            <img src='/assets/4.svg' alt='cant fine file!'></img>
+          </a>
+        </div>
+        <div className='txtCont'>
+          <div className='headerTxt headertxtstyle'>
+            <div>Let's give Jaguarz</div>
+          </div>
+          <div className=' subtxt headertxtstyle'>
+            the&nbsp; <span> future </span> &nbsp;they deserve
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
